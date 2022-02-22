34bf6517e15245192b0b248348c5cb47bbcc89fa
Valerie Remaker <44219004+vremaker@users.noreply.github.com>
2022-02-22 10:08:22 -0800

    added text

--- src/views/portfolio/night-owl.vue
+++ src/views/portfolio/night-owl.vue
@@ -1,6 +1,326 @@
 <template>
   <PortfolioPiece>
-</PortfolioPiece>
+    <div class="dci">
+      <div class="cow">
+        <img class="sm-img" src="/assets/tiles/night-owl.png" alt="DCI" />
+      </div>
+      <h1>Night Owl</h1>
+      <h3>Keeping Students Safe At Night</h3>
+
+      <p>
+        This project spanned 11 weeks in our HCDE 318 (Introduction to User
+        Centered Design) course, in Autumn 2019. As a team of 4, we applied the
+        user centered design process to the problem area of the danger of
+        walking alone at night for students. The process culminated in a mobile
+        application called Night Owl that gives students confidence and control
+        when walking alone at night.
+      </p>
+
+      <h3>PROBLEM STATEMENT</h3>
+      <p>
+        University District is known to be a sketchy neighborhood. For example,
+        the Safeway northwest of campus is fondly called “Sketchway” by
+        students. This brings up the concern that walking near campus at night
+        can be dangerous for students; students should not have to be concerned
+        with their safety when walking around their university. This problem
+        resonates with our team and peers, which is why we wanted to tackle the
+        area of how to help students feel safe and confident walking near UW’s
+        campus during night.”
+      </p>
+
+      <h3>INTERVIEWS</h3>
+      <p>
+        Our research consisted of three different methods: semi-structured
+        interview, “fly on the wall” observation, and competitive analysis.
+        Through these methods, we learned more about our user group and their
+        goals and pain points as well as other products already on the market.
+        Each team member conducted a one-on-one interview with a student at the
+        University of Washington that walks during dusk or at night at least 4
+        times a month. We then regrouped and synthesized our findings into key
+        takeaways that would help us define our problem space and user group.
+      </p>
+
+      <h4>Key Takeaways:</h4>
+      <ul>
+        <li>
+          They felt more comfortable talking with a friend on the phone when
+          walking alone
+        </li>
+        <li>Walking home alone is unavoidable</li>
+        <li>They know about UW safety, but doesn’t know how it works</li>
+        <li>They are very aware of their surroundings</li>
+      </ul>
+      <h4>Individual Interviews:</h4>
+      <ul>
+        <li>
+          <a
+            href="https://drive.google.com/file/d/1VaVEy4UpKD1jpLRqlOqRArkhunDgciJn/view"
+            target="_blank"
+            >Maya's Interviews</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://drive.google.com/file/d/1CZLo_7Qzbt-jNqwxZFSeKI0GEpFxEXrz/view"
+            target="_blank"
+            >Valerie's Interviews</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://drive.google.com/file/d/1fU0MwbjINa_bEsWx1NxDP3ozXNGpl8Gb/view"
+            target="_blank"
+            >Peter's Interviews</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://drive.google.com/file/d/1oPi_3ShrLl2moqErynGaPI06bK3blrU5/view"
+            target="_blank"
+            >Lia's Interviews</a
+          >
+        </li>
+      </ul>
+
+      <h3>OBSERVATIONS</h3>
+      <p>
+        Next, as a team we performed a “fly on the wall” observation on the Ave.
+        The goal of this study was to observe and investigate the actions and
+        behaviors of students walking in the U District at night.
+      </p>
+      <img
+        src="assets/night-owl/street.png"
+        alt="the corner of 45th and University Ave in Seattle's University District"
+      />
+      <h4>Key Findings:</h4>
+      <ul>
+        <li>Users were interacting with their phone in some way</li>
+        <li>Users walking alone didn’t seem scared</li>
+        <li>Many users wore earbuds</li>
+      </ul>
+      <a
+        href="https://drive.google.com/file/d/1bnC4-I8SMbEtcSESTvVFnjK3JHV3tf9V/view"
+        target="_blank"
+        >Find the full observation document here</a
+      >
+      <h3>COMPETITIVE ANALYSIS</h3>
+      <p>
+        Each team member conducted one competitive analysis on an existing
+        safety app. This allowed us to understand how existing products on the
+        market meet users’ needs and how they could be improved. We analyzed the
+        apps BSafe, Circle of 6, Sekura, and Noonlight. Overall, the apps all
+        had streamlined designs that generally made users feel safe, but also
+        had shortcomings such as confusing features and allowing users to easily
+        trigger emergency services on accident. We used this information to seek
+        opportunities, inspire our designs, and learn what to avoid.
+      </p>
+      <ul>
+        <li>
+          <a
+            href="https://drive.google.com/file/d/1sDMEpPfBFo0-krbwmo9DoYzp0f8WAvhV/view"
+            target="_blank"
+            >Sekura</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://drive.google.com/file/d/1tvpkDkMTISZ-MNfCNf2zx9QFNRNRkt-U/view"
+            target="_blank"
+            >Circle of 6</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://drive.google.com/file/d/1DqlQuCGTVTGIyUZcVeH58XN7XZwvyxlB/view"
+            target="_blank"
+            >Noonlight</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://drive.google.com/file/d/1aQa9VEE2RjTCpqlPM8jPYKB7LMkwHcLN/view"
+            target="_blank"
+            >BSafe</a
+          >
+        </li>
+      </ul>
+      <h3>USER PERSONAS</h3>
+      <p>
+        From our interviews, we gathered key insights on our users that we
+        compiled into categories such pain points, desires, and goals. These
+        helped us create personas grounded in data in order to avoid stereotypes
+        and assumptions. We chose to create two personas: one that was more
+        experienced with the U-District area and one that was only semi-familiar
+        with the area. This let us understand different user perspectives. Our
+        personas were a home base that we frequently visited to realign our
+        product and goals to our users’ needs.
+      </p>
+      <img src="assets/night-owl/amanda.png" alt="Amanda Persona" />
+      <img src="assets/night-owl/mallory.png" alt="Mallory Persona" />
+      <a
+        href="https://drive.google.com/file/d/1ezL8OijzPyig5U1rsJ2Ad_570KwMN8wk/view"
+        target="_blank"
+        >Find the full persona document here</a
+      >
+      <h3>USER JOURNEY MAPS</h3>
+      <p>
+        Our user journey maps represent the experiences of Amanda and Mallory,
+        two of our users, while walking home at night. We captured key touch
+        points between her and her environment and emotions throughout her walk
+        home. We focused on anxiety, alertness, and confidence as the emotions
+        we wanted to track, because they were the most prevalent emotions of our
+        interviewees. This journey map helped us understand our user’s feelings
+        and interactions while walking home.
+      </p>
+      <img src="assets/night-owl/amanda-map.jpeg" alt="amanda map" />
+      <img src="assets/night-owl/mallory-map.jpeg" alt="mallory map" />
+
+      <h3>DESIGN REQUIREMENTS</h3>
+      <p>
+        After better understanding our users and problem space through research,
+        we defined design requirements that outlined the functions of our
+        application. These requirements ensured that our solution solved user
+        pain points and satisfied their goals. In addition, they helped us
+        decide to focus on creating a phone app as our final product because it
+        would be able to fulfill the requirements. This would also be convenient
+        for our users since we observed that many of them interacted with their
+        phone in some way while walking at night.
+      </p>
+      <img src="assets/night-owl/aoc.png" alt="action object context" />
+      <img src="assets/night-owl/dfc.png" alt="data function contextual" />
+      <a
+        href="https://drive.google.com/file/d/1ahZiK5IC7KIrzcDx4orITnnQlw1ixcCK/view"
+        target="_blank"
+        >Find the full design requirements document here</a
+      >
+
+      <h3>STORYBOARDS</h3>
+      <p>
+        Each team member created two storyboards: one sketched and one photo or
+        photo hybrid. Each storyboard showed a specific scenario of our user
+        interacting with our potential product. It helped us visualize how our
+        user would interact with our potential solution. We chose to have each
+        team member focus on different user scenarios in order for us to
+        understand the full picture of the interactions between the user and our
+        product from the start to end of their walk.
+      </p>
+      <iframe
+        src="https://docs.google.com/presentation/d/e/2PACX-1vT7ZRiJRZz594YqNqFUgtkYilx3Y_mjmv6bztgaAo1AIPne7GY04ilbbpe-tWDHplDHZukXStqlQkTu/embed?start=true&loop=false&delayms=60000"
+        frameborder="0"
+        width="100%"
+        height="569"
+        allowfullscreen="true"
+        mozallowfullscreen="true"
+        webkitallowfullscreen="true"
+      ></iframe>
+
+      <h3>INFORMATION ARCHITECTURE</h3>
+      <p>
+        After deciding to make an app as our product, we started brainstorming
+        key features that would be the most helpful to our users. We utilized
+        our design requirements and user personas when ideating the app’s
+        features and how users would go through a specific task. Overall, our
+        information architecture showed the relationships and flows between each
+        feature. This set us up for success as we started ideating and creating
+        paper prototypes of our app screens.
+      </p>
+      <img src="assets/night-owl/ia.png" alt="information architecture" />
+      <h3>PAPER PROTOTYPING USABILITY TESTING</h3>
+      <p>
+        The paper prototypes were a quick, easy way to test our designed
+        features without distracting users with colors and photos. They helped
+        us pinpoint confusing and unintuitive aspects of our initial features,
+        which we incorporated into our redesign. First, we each individually
+        sketched and described three interfaces. Then, we came back together and
+        combined our rough draft sketches into consistent interface sketches for
+        our usability tests. Based on our storyboards and information
+        architecture diagram, we defined three key tasks that we asked users to
+        complete in our tests.
+      </p>
+      <p>
+        <em>Task 1:</em>User searches for a final destination, which will be
+        home. Then they will select the path that they want to take and start
+        their route.
+      </p>
+      <p>
+        <em>Task 2:</em>User reports a burglary incident and inputs the correct
+        information (date, time, description) and submits it. After submitting,
+        check that it was successfully added to the map.
+      </p>
+      <p><em>Task 3:</em>User adds new safety contact and then calls them.</p>
+      <h4>Participants:</h4>
+      <p>
+        We tested our prototype with four female University of Washington
+        students who walk alone at night at least once a week.
+      </p>
+      <h4>Method:</h4>
+      <p>
+        For each evaluation, we followed three major phases of testing in order
+        to cohesively gather quality information about our users and our app.
+        The first phase started off with an interview with each participant in
+        order to gather basic demographic information. The second phase
+        consisted of the main usability testing by providing users with a task
+        that they had to accomplish while observers recorded notes. During the
+        test, we asked participants to think out loud so that we could better
+        understand their thought process and pain points. To make it seem like a
+        real application, we used a Marvel App to conduct our usability testing.
+        Lastly, we performed a post test interview to get final details of what
+        participants thought of their experience of our solution to night
+        safety. After individually completing our usability tests, we came back
+        together to synthesize and analyze our findings. We found commonalities
+        in the features users struggled with, strengths and weaknesses of our
+        design, and suggestions for redesign.
+      </p>
+      <h4>Key Findings</h4>
+      <ul>
+        <li>
+          Users wanted to be able to share their location even when they hadn’t
+          started a path.
+        </li>
+        <li>
+          Users did not expect the app to immediately call a contact after
+          clicking on their contact tile.
+        </li>
+        <li>
+          Users want to be able to quickly input information when reporting a
+          hazard.
+        </li>
+        <li>
+          Users stated that hazards have different urgency levels, which should
+          be reflected in the app.
+        </li>
+      </ul>
+      <div class="d-flex flex-column">
+        <a
+          href="https://drive.google.com/file/d/1qPIgWAavYumI0R3QtWMOUUZ1rtpOxKA_/view"
+          >Find the full usability test document here</a
+        >
+        <a
+          href="https://drive.google.com/file/d/1wC734_OE-8ysEgzNrrHeQwgDfsHXpCfU/view"
+          >Find the full interface sketches document here</a
+        >
+      </div>
+      <h3>REFINEMENTS</h3>
+      <p>
+        Based on our findings from our usability tests we made some changes to
+        our designs. Below are the three main changes that we made based on user
+        feedback.
+      </p>
+      <h4>Report a Hazard:</h4>
+      <img src="/assets/night-owl/report.png" alt="report a hazard" />
+      <p>
+        Having different icon categories and hazard types was confusing, but users still wanted hazards to be differentiated in some way. We made a few changes based on this feedback:
+        </p>
+        <ul>
+          <li>Removed different icon types and only allowed users to report hazards</li>
+          <li>Removed hazard types</li>
+          <li>Allowed users to enter a title for the hazard</li>
+          <li>Added hazard levels (low, medium, and high danger)</li>
+          </ul>
+        <h4>Call</h4>
+
+    </div>
+  </PortfolioPiece>
 </template>
 
 <script>
