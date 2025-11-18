29eb5c4c68c64b71b25329fda4c62c2f5edd20d9
Valerie Remaker <44219004+vremaker@users.noreply.github.com>
2025-11-17 20:26:56 -0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -49,7 +49,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(process.env.BASE_URL),   // FIXED → hash mode for GitHub Pages
+  history: createWebHistory(process.env.BASE_URL),   // FIXED → hash mode for GitHub Pages
   routes,
 
   scrollBehavior(to, from, savedPosition) {
