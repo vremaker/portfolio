1cc2f4020c2d5f28f4c29c96a492542808ab996e
Valerie Remaker <44219004+vremaker@users.noreply.github.com>
2025-11-17 18:05:22 -0800

    testing deploy

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,9 @@ npm run build
 cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
+
+echo 'valerieremaker.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
